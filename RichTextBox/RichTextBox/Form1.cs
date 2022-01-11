0686da4edfc7c20c0d6cb4781557e44338dc0a9a
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-01-11 10:21:26 +0100

    Add files via upload

---
+++ RichTextBox/RichTextBox/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCitaj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBrisi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSpremi_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(Environment.SpecialFolder.MyDocuments + @"C:\TempProba.rtf");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonMapa_Click(object sender, EventArgs e)
+        {
+            string mapa = "TekstRichText";
+            if (!Directory.Exists(Environment.SpecialFolder.MyDocuments + mapa))
+            {
+                Directory.CreateDirectory(Environment.SpecialFolder.MyDocuments + mapa);
+            }
+
+        }
+    }
+}
